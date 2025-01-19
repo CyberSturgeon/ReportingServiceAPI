3baff68541ea9de67ef7bf9b2888d893cc55e5b0
Nikolay <tentotennot@mail.ru>
2025-01-19 12:12:16 +0300

    + config

---
+++ ReportingService.Persistence/Configuration/ComissionEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReportingService.Persistence.Entities;
+
+namespace ReportingService.Persistence.Configuration;
+
+public class ComissionEntityConfiguration : IEntityTypeConfiguration<Comission>
+{
+    public void Configure(EntityTypeBuilder<Comission> builder)
+    {
+        builder.Property(x => x.Id)
+            .IsRequired()
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Income)
+            .IsRequired();
+
+        builder.Property(x => x.Transaction)
+            .IsRequired();
+
+        builder.Property(x => x.TransactionId)
+            .IsRequired();
+
+        builder.HasOne(x => x.Transaction);
+    }
+}
