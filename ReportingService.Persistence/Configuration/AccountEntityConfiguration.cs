5c2217cd1a5cc8d05c71fc471176c53b3ebd2204
flyaka <flyaka1991@mail.ru>
2025-01-23 16:49:18 +0300

    add ConfigurationAccount

---
+++ ReportingService.Persistence/Configuration/AccountEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReportingService.Persistence.Entities;
+
+namespace ReportingService.Persistence.Configuration;
+
+internal static class AccountEntityConfiguration
+{
+    internal static void ConfigureAccount(this ModelBuilder builder)
+	{
+        builder.Entity<Account>().Property(x => x.Id)
+        .IsRequired()
+        .ValueGeneratedOnAdd();
+
+        builder.Entity<Account>().Property(x => x.CustomerId)
+       .IsRequired()
+
+        builder.Entity<Account>().Property<DateCreated>(x => x.CurrencyDate);
+
+        uilder.Entity<Account>().Property(x => x.Status)
+        .IsRequired();
+
+        builder.Entity<Account>().Property(x => x.Currensy)
+        .IsRequired();
+
+        builder.Entity<Account>().Property(x => x.Customer)
+        .IsRequired();
+
+        builder.Entity<Customer>().HasMany(x => x.Transactions)
+        
+    }
+}
