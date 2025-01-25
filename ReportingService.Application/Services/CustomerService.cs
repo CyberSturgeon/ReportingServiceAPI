b38449172f84c4e6d22438a7dd16f509da43888d
Nikolay <tentotennot@mail.ru>
2025-01-26 00:01:27 +0300

    WIP

--- ReportingService.Application/Services/CustomerService.cs
+++ ReportingService.Application/Services/CustomerService.cs
@@ -1,6 +1,23 @@
 ﻿
+using AutoMapper;
+using ReportingService.Application.Exceptions;
+using ReportingService.Application.Models;
+using ReportingService.Persistence.Repositories.Interfaces;
+
 namespace ReportingService.Application.Services;
 
-public class CustomerService ()
+public class CustomerService (
+        ICustomerRepository customerRepository,
+        IMapper mapper)
 {
+    public async Task<CustomerModel> GetCustomerByIdAsync(Guid id)
+    {
+        var customer = await customerRepository.GetByIdAsync(id) ??
+            throw new EntityNotFoundException($"Customer {id} not found");
+
+        var customerModel = mapper.Map<CustomerModel>(customer);
+
+        return customerModel;
+    }
+
 }
