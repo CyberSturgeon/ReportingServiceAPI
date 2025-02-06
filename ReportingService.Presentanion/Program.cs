39d2fbd6c6c236e8194a7010a3b8157c332878f2
Nikolay <tentotennot@mail.ru>
2025-02-07 02:29:27 +0300

    add swagger

--- ReportingService.Presentanion/Program.cs
+++ ReportingService.Presentanion/Program.cs
@@ -1,11 +1,11 @@
-using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
@@ -13,8 +13,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference(); // scalar/v1
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
