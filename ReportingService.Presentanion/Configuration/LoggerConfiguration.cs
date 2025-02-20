d3df46ead1ad59a17c691ee24667d14109d2fcc6
Nikolay <tentotennot@mail.ru>
2025-02-20 21:16:29 +0300

    configure serilog libs

--- ReportingService.Presentanion/Configuration/LoggerConfiguration.cs
+++ ReportingService.Presentanion/Configuration/LoggerConfiguration.cs
@@ -1,15 +1,15 @@
-﻿//using Serilog;
+﻿using Serilog;
 
-//namespace ReportingService.Presentanion.Configuration;
+namespace ReportingService.Presentanion.Configuration;
 
-//public static class LoggerConfiguration
-//{
-//    public static void ConfigureCustomLogging(this IHostBuilder host)
-//    {
-//        host.UseSerilog((context, configuration) =>
-//        {
-//            configuration
-//                .ReadFrom.Configuration(context.Configuration);
-//        });
-//    }
-//}
+public static class LoggerConfiguration
+{
+    public static void ConfigureCustomLogging(this IHostBuilder host)
+    {
+        host.UseSerilog((context, configuration) =>
+        {
+            configuration
+                .ReadFrom.Configuration(context.Configuration);
+        });
+    }
+}
