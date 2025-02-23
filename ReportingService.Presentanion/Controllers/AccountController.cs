f559381f0b1d5cc0ab50e7bb2ae1b13ea7a1dab8
flyaka <flyaka1991@mail.ru>
2025-02-23 23:00:29 +0300

    Add account service and controller

---
+++ ReportingService.Presentanion/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using ReportingService.Application.Services;
+using ReportingService.Application.Services.Interfaces;
+using ReportingService.Core.Configuration.Filters;
+using ReportingService.Presentanion.Models;
+
+namespace ReportingService.Presentanion.Controllers
+{
+    [Route("api")]
+    public class AccountController(
+    AccountService accountService,
+    IMapper mapper) : Controller
+    {
+
+        [HttpGet("accounts")]
+        public async Task<ActionResult<List<AccountResponse>>> GetAccountsByCustomerIdAsync([FromQuery] Guid customerId)
+        {
+            var accounts = await accountService.GetAccountsByCustomerIdAsync(customerId);
+            
+            var response = mapper.Map<List<AccountResponse>>(accounts);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ ReportingService.Presentanion/Models/AccountResponse.cs
@@ -0,0 +1,14 @@
+﻿using ReportingService.Core.Configuration;
+using ReportingService.Persistence.Entities;
+
+namespace ReportingService.Presentanion.Models
+{
+    public class AccountResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool Status { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
